2b48c6d1248088773687fa909a6278be4f5a1d2d
mihai <mihai@eessidev24>
2020-01-16 19:01:31 +0100

    Added as4 client

---
+++ AS4.Tests/As4ClientTests.cs
@@ -0,0 +1,39 @@
+﻿using AS4.Serialization;
+using AS4.Tests.Factories;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+namespace AS4.Tests
+{
+    [TestClass]
+    public class As4ClientTests
+    {
+        [TestMethod]
+        public void Bar()
+        {
+            string baseUrl = "http://localhost:12346/";
+            using (WebApp.Start(baseUrl, app => app.Run(Foo)))
+            {
+                var client = new As4Client();
+            
+                var message = new As4Message();
+                message.Set(Envelopes.UserMessage);
+            
+                var result = client.Send(new Uri(baseUrl), message);
+            }
+        }
+
+        private Task Foo(IOwinContext context)
+        {
+            var message = new As4Message();
+            message.Set(Envelopes.Receipt);
+            var mimeMessage = As4MessageToMimeEntity.Serialize(message);
+            context.Response.ContentType = mimeMessage.ContentType.MimeType + mimeMessage.ContentType.Parameters;
+            return mimeMessage.WriteToAsync(context.Response.Body, true);
+        }
+    }
+}
---
+++ AS4/As4Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using AS4.Serialization;
+using MimeKit;
+
+namespace AS4
+{
+    public class As4Client
+    {
+        public As4Message Send(Uri uri, As4Message message)
+        {
+            var mimeMessage = As4MessageToMimeEntity.Serialize(message);
+
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "POST";
+            request.ContentType = mimeMessage.ContentType.MimeType + mimeMessage.ContentType.Parameters;
+            
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                mimeMessage.WriteTo(requestStream, true);
+            }
+    
+            var response = (HttpWebResponse)request.GetResponse();
+            var contentType = ContentType.Parse(response.ContentType);
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                var mimeEntity = MimeEntity.Load(contentType, responseStream);
+                return MimeEntityToAs4Message.Deserialize(mimeEntity);
+            }
+        }
+    }
+}
