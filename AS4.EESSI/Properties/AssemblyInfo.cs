e2f0139b708fad82f0677e0928fe0039ffe30e61
mihai <mihai@eessidev24>
2020-01-21 13:56:38 +0100

    Update nuget packages

--- AS4.EESSI/Properties/AssemblyInfo.cs
+++ AS4.EESSI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EESSI.AS4")]
+[assembly: AssemblyTitle("AS4.EESSI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EESSI.AS4")]
+[assembly: AssemblyProduct("AS4.EESSI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
