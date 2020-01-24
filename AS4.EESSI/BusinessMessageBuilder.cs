d72f491bce6e401b384d7727dd6332eeaabd9122
mihai <mihai@eessidev24>
2020-01-24 15:51:35 +0100

    Added message builders

---
+++ AS4.EESSI/BusinessMessageBuilder.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using AS4.EESSI.Endpoints;
+using AS4.EESSI.Security;
+using AS4.Soap;
+
+namespace AS4.EESSI
+{
+    public class BusinessMessageBuilder
+    {
+        public Institution Sender { get; set; }
+        public Institution Receiver { get; set; }
+        public Attachment Sed { get; set; }
+        
+        public As4Message Build()
+        {
+            var envelope = CreateSoapEnvelope();
+            envelope.Header.Messaging.UserMessage.PartyInfo.From.PartyId.Value = Sender.Code;
+            envelope.Header.Messaging.UserMessage.PartyInfo.To.PartyId.Value = Receiver.Code;
+
+            
+            
+            
+            Sed.Stream.Position = 0;
+            var compressedStream = new MemoryStream();
+            using (var gzip = new GZipStream(compressedStream, CompressionMode.Compress, true))
+            {
+                Sed.Stream.CopyTo(gzip);
+            }
+            Sed.Stream = compressedStream;
+
+            envelope.Header.Messaging.UserMessage.PayloadInfo = new PayloadInfo
+            {
+                PartInfo = new PartInfo
+                {
+                    Reference = $"cid:{Sed.ContentId}",
+                    PartProperties = new[]
+                    {
+                        new Property {Name = "PartType", Value = "SED"},
+                        new Property {Name = "MimeType", Value = "application/xml"},
+                        new Property {Name = "CompressionType", Value = "application/gzip"}
+                    }
+                }
+            };
+
+            var message= new As4Message();
+            message.Set(envelope);
+            message.Attachments.Add(Sed);
+
+            Sed.Stream.Position = 0;
+            var ebmsSigner = new EbmsSigner
+            {
+                Xml = message.SoapEnvelope,
+                Certificate = Sender.Ebms, 
+                Uris = new [] {envelope.Header.Messaging.Id, envelope.Body.Id},
+                Attachments = message.Attachments
+            };
+            ebmsSigner.Sign();
+            
+            return message;
+        }
+
+        private Envelope CreateSoapEnvelope()
+        {
+            return new Envelope
+            {
+                Header = new Header
+                {
+                    Messaging = new Messaging
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserMessage = new UserMessage
+                        {
+                            MessageInfo = new MessageInfo
+                            {
+                                Timestamp = DateTime.Now,
+                                MessageId = Guid.NewGuid() + "@" + Environment.MachineName
+                            },
+                            PartyInfo = new PartyInfo
+                            {
+                                From = new Party
+                                {
+                                    PartyId = new PartyId
+                                    {
+                                        Type = "urn:eu:europa:ec:dgempl:eessi:ir",
+                                        Value = string.Empty
+                                    },
+                                    Role = "urn:eu:europa:ec:dgempl:eessi:ir:institution"
+                                },
+                                To = new Party
+                                {
+                                    PartyId = new PartyId
+                                    {
+                                        Type = "urn:eu:europa:ec:dgempl:eessi:ir",
+                                        Value = string.Empty
+                                    },
+                                    Role = "urn:eu:europa:ec:dgempl:eessi:ir:institution"
+                                }
+                            },
+                            CollaborationInfo = new CollaborationInfo
+                            {
+                                Service = new Service
+                                {
+                                    Type = "urn:eu:europa:ec:dgempl:eessi",
+                                    Value = "BusinessMessaging"
+                                },
+                                Action = "Send",
+                                ConversationId = Guid.NewGuid().ToString()
+                            },
+                            
+                        }
+                    }
+                },
+                Body = new Body
+                {
+                    Id = "body-id"
+                }
+            };
+        }
+    }
+}
---
+++ AS4.EESSI/SedBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Xml;
+using AS4.EESSI.Endpoints;
+using AS4.EESSI.Security;
+using AS4.Soap;
+
+namespace AS4.EESSI
+{
+    public class SedAttachmentBuilder
+    {
+        public XmlDocument Xml { get; set; }
+        public Institution Sender { get;set; }
+
+        public Attachment Build()
+        {   
+            var sedXml = GetSed(Xml);
+
+            var reader = new XmlNodeReader(sedXml);
+            var doc = new XmlDocument {PreserveWhitespace = true};
+            doc.Load(reader);
+
+            var xadesSigner = new XadesSigner
+            {
+                Xml = doc,
+                Certificate = Sender.Business
+            };
+            xadesSigner.Sign();
+
+            Xml.DocumentElement.RemoveChild(sedXml);
+            var newNode = Xml.ImportNode(doc.DocumentElement, true);
+            Xml.DocumentElement.AppendChild(newNode);
+            
+            var sedStream = new MemoryStream();
+            Xml.Save(sedStream);
+            sedStream.Position = 0;
+
+            return new Attachment
+            {
+                ContentId = "DefaultSED",
+                ContentType = "application/gzip",
+                Stream = sedStream
+            };
+        }
+
+        private XmlElement GetSed(XmlDocument xml)
+        {
+            foreach (XmlNode child in xml.DocumentElement.ChildNodes)
+            {
+                var element = child as XmlElement;
+                if (element != null && element.LocalName != "StandardBusinessDocumentHeader")
+                {
+                    return element;
+                }
+            }
+
+            return null;
+        }
+    }
+}
